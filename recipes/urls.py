230f1dd316c0cd4cde1b63f23a9178171b5d55ec
Allan Ribeiro da Silva <allanribeirodasilva@hotmail.com>
2023-07-27 01:03:19 -0300

    urls att

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from recipes.views import home, sobre, contato
+from recipes.views import home
 
 
 urlpatterns = [
-    path("sobre/", sobre),
     path("", home),
-    path("contato/", contato),
 ]
