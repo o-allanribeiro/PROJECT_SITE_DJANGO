15ed9667716cbbec31ff6530c50a1697344fd987
Allan Ribeiro da Silva <allanribeirodasilva@hotmail.com>
2023-07-29 11:45:02 -0300

    ajuste-3252

--- recipes/views.py
+++ recipes/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "recipes/home.html")
+    return render(
+        request, "recipes/Pages/home.html", context={"name": "Allan Ribeiro da Silva"}
+    )
