58824d0af84c572849ef844bed67e25a47b02b1d
Allan Ribeiro da Silva <allanribeirodasilva@hotmail.com>
2023-08-02 14:58:19 -0300

    copyorigin

--- projetosite/urls.py
+++ projetosite/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for projetosite project.
+"""projeto URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("recipes.urls")),
-    path("recipes/", include("recipes.urls")),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
-# Create your views here.
 def home(request):
-    return render(
-        request, "recipes/Pages/home.html", context={"name": "Allan Ribeiro da Silva"}
-    )
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
